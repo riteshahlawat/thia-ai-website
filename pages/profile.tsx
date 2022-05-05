44cfae44cbecf36377e7688821f905012683e11d
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-05-05 04:10:46 -0400

    route list update

---
+++ pages/profile.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const Profile: NextPage = () => {
+	return <div>Profile</div>;
+};
+
+export default Profile;
---
+++ pages/signup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const SignUp: NextPage = () => {
+	return <div>SignUp</div>;
+};
+
+export default SignUp;
