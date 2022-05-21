6ddc970769e0535cc858bc0e911ef74fc2c862c8
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-05-21 17:25:53 -0400

    ENG-166: Thia theme chosen, button styles updated

---
+++ styles/components/button.ts
@@ -0,0 +1,44 @@
+import { ComponentStyleConfig } from '@chakra-ui/react';
+import { mode, darken } from '@chakra-ui/theme-tools';
+
+export const Button: ComponentStyleConfig = {
+  // Styles for the base style
+  baseStyle: {
+    _focus: { boxShadow: 'none' },
+    borderRadius: '3xl',
+    transition: '250ms',
+  },
+  // Styles for the size variations
+  sizes: {},
+  // Styles for the visual style variations
+  variants: {
+    primary: (props: any) => ({
+      bg: mode('thia.purple.50', 'thia.purple.700')(props),
+      color: mode('thia.purple.900', 'white')(props),
+      _hover: { bg: mode('thia.purple.100', 'thia.purple.600')(props), borderRadius: 'xl' },
+      _active: { bg: mode('thia.purple.200', 'thia.purple.700')(props) },
+    }),
+    primaryOutline: (props: any) => ({
+      bg: 'transparent',
+      color: mode('thia.text-base', 'thia.text-dark')(props),
+      _hover: { bg: mode('thia.purple.50', 'thia.purple.900')(props), borderRadius: 'xl' },
+      _active: { bg: mode('thia.purple.100', 'thia.purple.800')(props) },
+      outline: mode('2px solid var(--purple-200)', '2px solid var(--purple-600)')(props),
+    }),
+    secondary: (props: any) => ({
+      bg: mode('blackAlpha.100', 'whiteAlpha.100')(props),
+      _hover: { bg: mode('blackAlpha.200', 'whiteAlpha.200')(props), borderRadius: 'xl' },
+      _active: { bg: mode('blackAlpha.300', 'whiteAlpha.300')(props) },
+    }),
+    secondaryGhost: (props: any) => ({
+      bg: 'transparent',
+      _hover: { bg: mode('thia.gray.100', 'whiteAlpha.200')(props) },
+      _active: {
+        bg: mode('thia.gray.200', 'whiteAlpha.300')(props),
+      },
+    }),
+  },
+
+  // The default `size` or `variant` values
+  defaultProps: {},
+};
---
+++ styles/components/link.ts
@@ -0,0 +1,9 @@
+import { ComponentStyleConfig } from '@chakra-ui/react';
+
+export const Link: ComponentStyleConfig = {
+  baseStyle: {
+    _focus: {
+      boxShadow: 'none',
+    },
+  },
+};
