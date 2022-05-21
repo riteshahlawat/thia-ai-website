1e8dae42de9cc34afd3a0857878aa0a50f7f1e64
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-05-21 17:28:41 -0400

    ENG-167: Fixed mobile view

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -57,12 +57,17 @@ export default function Footer() {
               <Stack direction={'row'} spacing={6}>
                 {socials.map(({ label, path, icon }) => (
                   <IconButton
-                    key={label}
                     as='a'
+                    key={label}
                     href={path}
+                    icon={icon}
                     rounded='full'
                     aria-label={label}
-                    icon={icon}
+                    variant='secondary'
+                    _hover={{
+                      borderRadius: '3xl',
+                      bg: useColorModeValue('thia.gray.200', 'thia.gray.900'),
+                    }}
                   ></IconButton>
                 ))}
               </Stack>
@@ -101,18 +106,14 @@ export default function Footer() {
             <VStack align={'flex-start'}>
               <ListHeader>Subscribe to our newsletter</ListHeader>
               <HStack w='full'>
-                <Input placeholder={'Your email address'} variant='filled' />
-                <IconButton
-                  aria-label='Subscribe'
-                  bg={useColorModeValue('thia.purple-base', 'thia.purple-dark')}
-                  _hover={{
-                    bg: useColorModeValue('thia.purple-hover-base', 'thia.purple-hover-dark'),
-                  }}
-                  _active={{
-                    bg: useColorModeValue('thia.purple-hover.base', 'thia.purple-Hover-dark'),
-                  }}
-                  icon={<BiMailSend />}
+                <Input
+                  placeholder={'Your email address'}
+                  variant='filled'
+                  colorScheme='thia.gray'
+                  bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+                  _hover={{ bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200') }}
                 />
+                <IconButton aria-label='Subscribe' variant='primary' icon={<BiMailSend />} />
               </HStack>
             </VStack>
           </GridItem>
@@ -121,8 +122,18 @@ export default function Footer() {
         <Flex justify='space-between' py='8' fontSize='sm' gap={4}>
           <Text>© 2021 Thia CA, inc. All rights reserved</Text>
           <HStack spacing={{ base: 4, md: 8 }} align='start'>
-            <Link href={'#'}>Terms of Service</Link>
-            <Link href={'#'}>Privacy Policy</Link>
+            <Link href={'#'}>
+              Terms
+              <Box as='span' display={{ base: 'none', md: 'inline' }}>
+                {' of Service'}
+              </Box>
+            </Link>
+            <Link href={'#'}>
+              Privacy
+              <Box as='span' display={{ base: 'none', md: 'inline' }}>
+                {' Policy'}
+              </Box>
+            </Link>
             <Link href={'#'}>License</Link>
           </HStack>
         </Flex>
