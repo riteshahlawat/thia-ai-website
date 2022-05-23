931a00b02d661bba053eed56f9f110a362641954
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-05-23 04:12:43 -0400

    clean up

--- src/NextPageWithLayoutType.ts
+++ src/NextPageWithLayoutType.ts
@@ -1,7 +1,6 @@
-import { NextPage } from "next";
-import { ReactElement, ReactNode } from "react";
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
-
