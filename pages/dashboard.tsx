2299592fb863d27d07c9fb53021a59a5532a453d
David Chan <dcdaveychan11@gmail.com>
2022-05-29 22:54:54 -0400

    ENG-182: Fix dashboard prerendering bug

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuth, useSigninCheck, useUser } from 'reactfire';
 import { Box, Center, Spinner } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
@@ -6,33 +6,20 @@ import { useRouter } from 'next/router';
 type Props = {}
 
 const Dashboard = (props: Props) => {
-    const auth = useAuth();
-    const router = useRouter();
 	const { data: user } = useUser();
     const { status, data: signInCheckResult } = useSigninCheck();
-    const DisplayDashboard = () => {
-        if (status === "loading") {
-            return (
-                <Center w='100vw' h='100vh'>
-                    <Spinner />
-                </Center>
-            )
-        } 
-        if (signInCheckResult.signedIn === true) {
-            return (
-                <Box>Welcome, {user?.displayName}</Box>
-            )
-        } else {
+    const router = useRouter();
+    
+    useEffect(() => {
+        if (!user) {
             router.push('/login')
-            return (
-                <Box />
-            )
         }
-    }
+    })
 
     return (
-        <DisplayDashboard />
+        <Box>Welcome, {user?.displayName}</Box>
     )
+
 }
 
 export default Dashboard
