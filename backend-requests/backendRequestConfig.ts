fec1a46451d8c3ac76b3e336160666c4fccbac05
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-07-16 03:14:45 -0400

    bug fix

--- backend-requests/backendRequestConfig.ts
+++ backend-requests/backendRequestConfig.ts
@@ -6,9 +6,7 @@ export const BACKEND_URL_PROD = 'https://api.thia.tech';
 /**
  *	Configuration for Backend API requests
  */
-const BackendRequestConfig = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-});
+const BackendRequestConfig = axios.create({ baseURL: process.env.NEXT_PUBLIC_BACKEND_URL });
 
 // See https://stackoverflow.com/questions/27513994/chrome-stalls-when-making-multiple-requests-to-same-resource
 BackendRequestConfig.defaults.headers.common['Cache-Control'] = 'no-cache';
