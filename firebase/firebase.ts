722d9318d5b0b9d8f6bb29991e1ded53fa11efa3
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-07-16 13:04:54 -0400

    Updated firebase config

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,23 +1,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
-export const getFirebaseConfig = (): FirebaseOptions => {
-    if (process.env.NODE_ENV === 'development') {
-        return {
-            apiKey: 'AIzaSyAoiWNvP_NuTIoXQ-oghg3Ix9jWLblQXcU',
-            authDomain: 'thia-dev.firebaseapp.com',
-            projectId: 'thia-dev',
-            storageBucket: 'thia-dev.appspot.com',
-            messagingSenderId: '554589208456',
-            appId: '1:554589208456:web:33c36d197c8467b1f6443d',
-        };
-    } else
-        return {
-            apiKey: 'AIzaSyDJOaqbWCPHazZsu4uyXCcYl69bSzr-hdc',
-            authDomain: 'thia-1458b.firebaseapp.com',
-            projectId: 'thia-1458b',
-            storageBucket: 'thia-1458b.appspot.com',
-            messagingSenderId: '564483189530',
-            appId: '1:564483189530:web:62e754e4173dfb80fd1aa3',
-            measurementId: 'G-PFW73XVXV6',
-        };
-};
+export const getFirebaseConfig = (): FirebaseOptions => ({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
