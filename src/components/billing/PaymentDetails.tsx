a7eefdae5bb1f8014f0575396a242d0b39831ba2
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-07-24 01:54:01 -0400

    ENG-216: Improved ui for managing cards, added ability to add cards

--- src/components/billing/PaymentDetails.tsx
+++ src/components/billing/PaymentDetails.tsx
@@ -4,6 +4,8 @@ import {
     Button,
     Flex,
     FormControl,
+    FormLabel,
+    Heading,
     Input,
     Modal,
     ModalBody,
@@ -16,10 +18,12 @@ import {
     Text,
     useColorModeValue,
     useDisclosure,
+    VStack,
 } from '@chakra-ui/react';
 import { CardElement, Elements, PaymentElement } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
 import Stripe from 'stripe';
 import { BorderBox } from './BorderBox';
 
@@ -42,26 +46,20 @@ const CARD_ELEMENT_OPTIONS = {
 };
 
 export const PaymentDetails = ({ cardList, defaultCard }: { cardList: Stripe.PaymentMethod[]; defaultCard: string | null }) => {
-    const card = cardList[0]?.card;
-    const brand = card?.brand ?? '----';
-    const exp_month = card?.exp_month ?? '--';
-    const exp_year = card?.exp_year ?? '----';
-    const last4 = card?.last4 ?? '----';
-    const expiryDate = `${String(exp_month).padStart(2, '0')}/${exp_year}`;
-
-    const cardBGColor = useColorModeValue('thia.gray.50', 'thia.gray.950');
+    const cardBGColor = useColorModeValue('thia.gray.50', 'thia.gray.900');
     const secondaryTextColor = useColorModeValue('thia.gray.700', 'thia.gray.300');
 
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [addCard, setAddCard] = useState(false);
 
-    //   const { handleSubmit: addCardToUser, isCardSubmitting } = submitCardElement(
-    //     async () => {
-    //         onClose();
-    //         // await loadData();
-    //     },
-    //     onClose,
-    //     // defaultCardID
-    // );
+    const handleOnClose = () => {
+        setAddCard(false);
+        onClose();
+    };
+    const handleAddACard = () => {
+        setAddCard(true);
+    };
+    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
     return (
         <>
@@ -73,65 +71,111 @@ export const PaymentDetails = ({ cardList, defaultCard }: { cardList: Stripe.Pay
                             Update or edit your payment details
                         </Text>
                     </>
-                    <Box mt={5} p={5} rounded='lg' bg={cardBGColor}>
-                        <Flex gap={5}>
-                            {card ? (
-                                <>
-                                    <Box>{brand}</Box>
-                                    <Box flexGrow={1}>
-                                        <Text fontSize='sm' fontWeight='semibold' letterSpacing='wide'>
-                                            <Box as='span' textTransform='capitalize'>
-                                                {brand}
-                                            </Box>
-                                            {last4 ? ` ending in ${last4}` : <Skeleton height='20px' />}
-                                        </Text>
-                                        <Text fontSize='sm' color={secondaryTextColor}>{`Expiry ${expiryDate}`}</Text>
-                                        <Text></Text>
-                                    </Box>
-                                    <Button variant='secondary'>Edit</Button>
-                                </>
-                            ) : (
-                                <>
-                                    <Button variant='secondary' onClick={onOpen}>
-                                        Add card
-                                    </Button>
-                                </>
-                            )}
-                        </Flex>
-                    </Box>
+                    <Button variant='secondary' onClick={onOpen}>
+                        Manage payment methods
+                    </Button>
                 </Box>
             </BorderBox>
 
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+            <Modal isOpen={isOpen} onClose={handleOnClose} isCentered>
                 <ModalOverlay bg='blackAlpha.300' backdropFilter='blur(10px)' />
-                <ModalContent py={5} rounded='lg'>
-                    <ModalHeader>Add a card</ModalHeader>
+
+                <ModalContent py={5} rounded='xl'>
+                    <ModalHeader>
+                        <Heading>Payment Details</Heading>
+                    </ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <PayemntForm />
+                        {!addCard ? (
+                            <VStack spacing={5} align='start'>
+                                <VStack spacing={5} overflowY='auto' w='full'>
+                                    {cardList.map(({ card }, i) => (
+                                        <CardPreview key={i} card={card} />
+                                    ))}
+                                </VStack>
+                                <Heading fontSize={24}>Add a card</Heading>
+                                <Button w='full' variant='primary' onClick={handleAddACard}>
+                                    Add a card
+                                </Button>
+                            </VStack>
+                        ) : (
+                            <Elements stripe={stripePromise}>
+                                <PayemntForm defaultCard={defaultCard} />
+                            </Elements>
+                        )}
                     </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button variant='secondary'  colorScheme='thia.purple'>
-                            Add card
-                        </Button>
-                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
     );
 };
 
-const PayemntForm = () => {
-    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
+const CardPreview = ({ card }: { card: Stripe.PaymentMethod.Card | undefined }) => {
+    const brand = card?.brand ?? '----';
+    const exp_month = card?.exp_month ?? '--';
+    const exp_year = card?.exp_year ?? '----';
+    const last4 = card?.last4 ?? '----';
+    const expiryDate = `${String(exp_month).padStart(2, '0')}/${exp_year}`;
+    const cardBGColor = useColorModeValue('thia.gray.50', 'thia.gray.900');
+    const secondaryTextColor = useColorModeValue('thia.gray.700', 'thia.gray.300');
+
     return (
-        <Elements stripe={stripePromise}>
+        <Box w='full' p={5} rounded='xl' bg={cardBGColor}>
+            <Flex gap={5}>
+                <>
+                    <Box>{brand}</Box>
+                    <Box flexGrow={1}>
+                        <Text fontSize='sm' fontWeight='semibold' letterSpacing='wide'>
+                            {last4 ? ` **** **** **** ${last4}` : <Skeleton height='20px' />}
+                        </Text>
+                        <Text fontSize='sm' color={secondaryTextColor}>{`Expiry ${expiryDate}`}</Text>
+                        <Text></Text>
+                    </Box>
+                    <Button variant='secondary'>Edit</Button>
+                </>
+            </Flex>
+        </Box>
+    );
+};
+
+const PayemntForm = ({ defaultCard }: { defaultCard: string | null }) => {
+    const { handleSubmit: addCardToUser, isCardSubmitting } = submitCardElement(
+        async () => {
+            console.log('CARD ADDED');
+        },
+        () => {
+            console.log('CARD FAILED');
+        },
+        defaultCard
+    );
+    return (
+        <VStack pb={5} spacing={3}>
+            <Flex gap={5}>
+                <FormControl>
+                    <FormLabel>First Name</FormLabel>
+                    <Input type='email' />
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Last Name</FormLabel>
+                    <Input type='email' />
+                </FormControl>
+            </Flex>
             <FormControl>
-                <CardElement />
+                <FormLabel>Address</FormLabel>
+                <Input type='email' />
             </FormControl>
-        </Elements>
+            <FormControl>
+                <FormLabel>City </FormLabel>
+                <Input type='email' />
+            </FormControl>
+            <FormControl py={3}>
+                <FormLabel>Card information</FormLabel>
+
+                <CardElement options={CARD_ELEMENT_OPTIONS} />
+            </FormControl>
+            <Button w='full' variant='primary' onClick={addCardToUser} isLoading={isCardSubmitting} loadingText='...Adding card'>
+                Add card
+            </Button>
+        </VStack>
     );
 };
