711068a4f48c7475bf70f8aa4b593789c7700d3a
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-09-14 16:44:40 -0400

    removed unnecessary object nesting

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { UrlObject } from 'url';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 interface LinkObjType {
     [key: string]: {
-        path: string | UrlObject;
+        path: string;
         icon: React.ReactNode;
         label: string;
     };
 }
 
-export const links: { [key: string]: LinkObjType } = {
-    landing: { index: { path: { pathname: '/' }, icon: null, label: 'Home' } },
-    pricing: { index: { path: { pathname: '/pricing' }, icon: null, label: 'Pricing' } },
-    download: { index: { path: { pathname: '/download' }, icon: null, label: 'Download' } },
-    support: { index: { path: { pathname: '/support' }, icon: null, label: 'Support' } },
-    docs: { index: { path: { pathname: '/docs' }, icon: null, label: 'Docs' } },
-    changelog: { index: { path: { pathname: '/changelog' }, icon: null, label: 'Changelog' } },
+export const links: LinkObjType = {
+    landing: { path: '/', icon: null, label: 'Home' },
+    pricing: { path: '/pricing', icon: null, label: 'Pricing' },
+    download: { path: '/download', icon: null, label: 'Download' },
+    support: { path: '/support', icon: null, label: 'Support' },
+    docs: { path: '/docs', icon: null, label: 'Docs' },
+    changelog: { path: '/changelog', icon: null, label: 'Changelog' },
 };
 
 export const socials: LinkObjType = {
