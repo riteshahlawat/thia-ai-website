55ddd6c8bf35564e2a61849122885ff29242f228
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-10-04 02:47:23 -0400

    ENG-255: refactored nav element

--- src/components/layout/Navbar/NavContainer.tsx
+++ src/components/layout/Navbar/NavContainer.tsx
@@ -6,23 +6,20 @@ export const NavContainer = ({ children }: { children?: React.ReactNode }) => {
         <Box
             left={0}
             right={0}
-            as='header'
+            as='nav'
             zIndex={1000}
             position='fixed'
-            top={0}
             backdropFilter='auto'
             backdropBlur='8px'
             bg={useColorModeValue('whiteAlpha.800', 'blackAlpha.800')}
             borderBottom='1px solid'
             borderBottomColor={useColorModeValue('thia.gray.100', 'thia.gray.950')}
         >
-            <nav>
-                <ContentContainer pl={[6, 7, 8, 8, 8]} pr={[0, 0, 8, 8, 8]}>
-                    <HStack align='center' className='nav' justify='space-between' wrap='nowrap' w='100%'>
-                        {children}
-                    </HStack>
-                </ContentContainer>
-            </nav>
+            <ContentContainer pl={[6, 7, 8, 8, 8]} pr={[0, 0, 8, 8, 8]}>
+                <HStack align='center' className='nav' justify='space-between' wrap='nowrap' w='100%'>
+                    {children}
+                </HStack>
+            </ContentContainer>
         </Box>
     );
 };
