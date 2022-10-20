72ac79e57fb751aeb326e78e938e8592b64f0505
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-10-20 16:58:32 -0400

    ENG-280: fixed ssg for privacy policy

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import Image from 'next/image';
 import { ContentContainer } from '@/components/common/ContentContainer';
 import { SeoPage } from '@/components/seo/SeoPage';
 import { Text, useColorModeValue } from '@chakra-ui/react';
-import { allPrivacyPolicies } from 'contentlayer/generated';
+import { allPrivacyPolicies, PrivacyPolicy } from 'contentlayer/generated';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 import { Prose } from '@nikolovlazar/chakra-ui-prose';
+import { GetStaticProps } from 'next';
 
-const components = { Image };
+type Props = { doc: PrivacyPolicy };
 
-const PrivacyPolicy = () => {
-    const pp = allPrivacyPolicies[0];
+const PrivacyPolicy = ({ doc }: Props) => {
     const color = useColorModeValue('thia.gray.700', 'thia.gray.600');
-    const dateLastUpdated = new Date(pp.lastUpdatedAt).toLocaleDateString('default', { dateStyle: 'medium' });
-    const MDXComponent = useMDXComponent(pp.body.code);
+    const dateLastUpdated = new Date(doc.lastUpdatedAt).toLocaleDateString('default', { dateStyle: 'medium' });
+    const MDXComponent = useMDXComponent(doc.body.code);
 
     return (
         <SeoPage title='Privacy Policy' description='View our privacy policy'>
@@ -22,11 +21,15 @@ const PrivacyPolicy = () => {
                     Date Last Updated: {dateLastUpdated}
                 </Text>
                 <Prose as='article'>
-                    <MDXComponent components={components} />
+                    <MDXComponent />
                 </Prose>
             </ContentContainer>
         </SeoPage>
     );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    return { props: { doc: allPrivacyPolicies[0] } };
 };
 
 export default PrivacyPolicy;
